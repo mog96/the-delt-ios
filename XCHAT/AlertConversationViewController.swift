600758f633773f87ab8bcc6139ea2b366f0d573c
Mateo Garcia <mateog@stanford.edu>
2017-01-31 03:02:23 -0800

    Fixes misplaced completion block call in AlertConversationViewController.

--- XCHAT/AlertConversationViewController.swift
+++ XCHAT/AlertConversationViewController.swift
@@ -103,11 +103,11 @@ extension AlertConversationViewController {
                     
                     print("REPLIES:", replies)
                     
-                    completion(replies)
                 } else {
                     print("object is nil")
                     print(error!.localizedDescription)
                 }
+                completion(replies)
             }
         } else {
             print("ALERT OBJECT ID NOT FOUND")
